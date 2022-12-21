71c929251f8311e3895103533576bfe9f9b8b800
Viktor Vorobev <viktor.vorobev@emlid.com>
2022-12-21 18:05:54 +0100

    py: tests: search: add binary search for testing

--- py/tests/test_search.py
+++ py/tests/test_search.py
@@ -21,6 +21,7 @@ class FuncToTest:
 
 SEARCH_FUNCTIONS = [
     pytest.param(FuncToTest(func=search.linear_search, sort_required=False), id='linear_search'),
+    pytest.param(FuncToTest(func=search.binary_search, sort_required=True), id='binary_search'),
 ]
 
 
