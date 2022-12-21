6cbf82d322da1466c8d09cda04cfaf318fa394b6
Viktor Vorobev <viktor.vorobev@emlid.com>
2022-12-21 17:52:07 +0100

    py: tests: test search: fix data samples generation
    Now data samples are properly random from test to test. Previously data
    samples were enerated only once and would be sorted in place, which
    means that arrays were still sorted from the previous tests

--- py/tests/test_search.py
+++ py/tests/test_search.py
@@ -9,16 +9,17 @@ from algo import search
 
 MAX_VALUE = 1_000_000
 
-TEST_DATA = [
-    pytest.param(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), 100), id='small_even_len'),
-    pytest.param(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), 101), id='small_odd_len'),
-    pytest.param(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), 1_000_000), id='big_even_len'),
-    pytest.param(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), 1_000_001), id='big_odd_len'),
+SEARCH_FUNCTIONS = [
+    search.linear_search,
 ]
 
-SEARCH_FUNCTIONS = [
-    search.linear_search
-]
+
+@pytest.fixture(name='data_sample',
+                params=[10, 11, 1_000_000, 1_000_001],
+                ids=['small_even_len', 'small_odd_len', 'big_even_len', 'big_odd_len'])
+def data_sample_fixture(request):
+    """Fixture that generates data sample"""
+    return list(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), request.param))
 
 
 @pytest.fixture(name='should_be_found', params=[True, False], ids=['should_found', 'should_not_found'])
@@ -34,31 +35,30 @@ def should_be_sorted_fixture(request):
 
 
 @pytest.fixture(name='test_data')
-def test_data_fixture(should_be_found, should_be_sorted, request):
+def test_data_fixture(should_be_found, should_be_sorted, data_sample):
     """
     Fixture that prepares test data
     :param should_be_found: flag that shows whether returned target value should be findable or not
     :param should_be_sorted: flag that shows whether returned array should be sorted or not
-    :param request: request that contains test array as a parameter
+    :param data_sample: fixture test array as a parameter
     :return: array to search in, target value to search, expected result
     """
-    data = request.param
+
     if should_be_sorted:
-        data.sort()
+        data_sample.sort()
     if should_be_found:
-        expected_result = random.randint(0, len(data) - 1)
-        target_value = data[expected_result]
+        expected_result = random.randint(0, len(data_sample) - 1)
+        target_value = data_sample[expected_result]
     else:
         expected_result = None
         while True:
-            target_value = data[random.randint(0, len(data) - 1)]
+            target_value = data_sample[random.randint(0, len(data_sample) - 1)]
             target_value -= 1
-            if target_value not in data:
+            if target_value not in data_sample:
                 break
-    return request.param, target_value, expected_result
+    return data_sample, target_value, expected_result
 
 
-@pytest.mark.parametrize('test_data', TEST_DATA, indirect=True)
 @pytest.mark.parametrize('search_func', SEARCH_FUNCTIONS)
 def test_search(test_data, search_func):
     """Test function that covers all cases with search algorithms"""
