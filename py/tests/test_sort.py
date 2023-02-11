50ba88ed9ea630166b02ef5252dafa15444b1469
Viktor Vorobev <viktor.vorobev@emlid.com>
2023-02-11 11:36:37 +0100

    py: tests: test_sort: add tests for sorting algorithms

---
+++ py/tests/test_sort.py
@@ -0,0 +1,50 @@
+"""
+Tests for sort functions
+"""
+
+import random
+
+import pytest
+from algo import sort
+
+MAX_VALUE = 1_000_000
+
+SORT_FUNCTIONS = [
+    pytest.param(sort.bubble_sort, id='bubble_sort'),
+]
+
+
+@pytest.fixture(name='sort_func', params=SORT_FUNCTIONS)
+def sort_func_fixture(request):
+    """Fixture that returns test function with its meta information"""
+    return request.param
+
+
+SAMPLE_LENS = [0, 1, 2, 3, 4, 10, 11, 1000, 1001, 10_000, 10_001]
+
+
+@pytest.fixture(name='data_sample',
+                params=SAMPLE_LENS,
+                ids=[f'len_{i}' for i in SAMPLE_LENS])
+def data_sample_fixture(request):
+    """Fixture that generates array to sort"""
+    if not request.param:
+        return []
+    return list(random.sample(range(-1 * MAX_VALUE, MAX_VALUE), request.param))
+
+
+@pytest.fixture(name='test_data')
+def test_data_fixture(data_sample, sort_func):
+    """
+    Fixture that prepares test data
+    :param data_sample: fixture test array as a parameter
+    :return: sort function, array to sort, expected result
+    """
+    expected_result = sorted(data_sample)
+    return sort_func, data_sample, expected_result
+
+
+def test_search(test_data):
+    """Test function that covers all cases with sort algorithms"""
+    sort_func, data, expected_result = test_data
+    assert expected_result == sort_func(data)
