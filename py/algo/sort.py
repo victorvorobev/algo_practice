d580683c5fa49abe5f5f7251c87132a7adf8e028
Viktor Vorobev <viktor.vorobev@emlid.com>
2023-02-11 11:54:01 +0100

    py: algo: sort: add merge sort

--- py/algo/sort.py
+++ py/algo/sort.py
@@ -22,3 +22,37 @@ def bubble_sort(array: list[int]) -> list[int]:
         if not swapped:
             break
     return array
+
+
+def merge_sort(array: list[int]) -> list[int]:
+    """
+    Divide and conquer, O(nlog(n))
+    :param array: Array to sort
+    :return: sorted array
+    """
+    array_size = len(array)
+    if array_size <= 1:
+        return array
+
+    mid = array_size // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    result = []
+    l_index = r_index = 0
+
+    while l_index < len(left) and r_index < len(right):
+        if left[l_index] <= right[r_index]:
+            result.append(left[l_index])
+            l_index += 1
+        else:
+            result.append(right[r_index])
+            r_index += 1
+    while l_index < len(left):
+        result.append(left[l_index])
+        l_index += 1
+    while r_index < len(right):
+        result.append(right[r_index])
+        r_index += 1
+
+    return result
